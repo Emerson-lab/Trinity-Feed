20a87c02a4310ec2e95c96b0b62702369622b457
Emerson Trindade <emersont@portasabertas.org.br>
2022-07-26 22:44:58 -0300

    delete comment

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,12 +1,15 @@
 import { format, formatDistanceToNow } from 'date-fns';
 import ptBR from "date-fns/locale/pt-BR";
+import { useState } from 'react';
 import Avatar from "../Avatar";
 import Comment from "../Comment";
 import styles from "./Post.module.css";
 
 export default function Post(props) {
+  const [comments, setComments] = useState([]);
+  const [newCommentText, setNewCommentText] = useState('');
 
-  //formatar data com a lib date-fns
+  //formatar data com a date-fns lib
   const publishedDateFormatted =
     format(props.publishedAt,
       "d de LLLL 'às' HH:mm'h'",
@@ -14,11 +17,35 @@ export default function Post(props) {
         locale: ptBR
       }
     )
-  //tempo decorrido apos à data publicada do post
-  const publishedDateRelativeToNow = formatDistanceToNow(props.publishedAt, {
-    locale: ptBR,
-    addSuffix: true
-  })
+
+  //tempo decorrido apos a data publicada do post
+  const publishedDateRelativeToNow =
+    formatDistanceToNow(props.publishedAt,
+      {
+        locale: ptBR,
+        addSuffix: true
+      }
+    )
+
+  const handleCreateNewContent = () => {
+    event.preventDefault();
+
+    setComments([...comments, newCommentText]);
+    setNewCommentText('')
+   
+  }
+
+  const handleNewCommentChange = () => {
+    setNewCommentText(event.target.value)
+  }
+
+  const deletComment = (commentToDelete) => {
+    const commentWithoutDeletOne = comments.filter(comment => {
+    return comment !== commentToDelete
+    })
+
+    setComments(commentWithoutDeletOne)    
+  }
 
   //formatar data com javascript
   // const publishedDateFormatted = new Intl.DateTimeFormat('pt-BR', {
@@ -43,38 +70,47 @@ export default function Post(props) {
 
         <time
           title={publishedDateFormatted}
-          dateTime={publishedDateFormatted}
+          dateTime={props.publishedAt.toISOString()}
         >
           {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-        <p>👉{' '}<a href="">jane.design/doctorcare</a></p>
-
-        <p>
-          <a href="">#novoprojeto </a>{' '}
-          <a href="">#nlw </a>{' '}
-          <a href="">#rocketseat</a>{' '}
-        </p>
+        {props.content.map(line => {
+          if (line.type === 'paragraph') {
+            return <p key={line.content}>{line.content}</p>
+          } else if (line.type === 'link') {
+            return <p key={line.content}><a href={line.content}>{line.content}</a></p>
+          }
+        })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewContent} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
         <textarea
+          name="comment"
           placeholder="Deixe um comentário"
+          onChange={handleNewCommentChange}
+          value={newCommentText}
         />
+
         <footer>
           <button type="submit">Publicar</button>
         </footer>
       </form>
+
       <div className={styles.commentList}>
-        <Comment />
+        {comments.map(comment => {
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeletComment={deletComment}
+            />
+          )
+        })}
       </div>
 
     </article>
