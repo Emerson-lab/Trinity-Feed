8e0474910333f02bfe59527ddd4ff41540e55976
Emerson Trindade <trinddade29@gmail.com>
2022-10-24 20:42:00 -0300

    header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,14 @@
 import styles from './Header.module.css';
 
 import trnityLogo from '../../assets/trinity-logo.svg';
+import { useState } from 'react';
 
 export default function Header() {
+  const [open, setOpen] = useState(true);
+
   return (
     <header className={styles.header}>
-      <img src={trnityLogo} alt="lgotipo trnity developer"/>
+      <img src={trnityLogo} alt="lgotipo trnity developer" />
       <strong>Trinity Feed</strong>
     </header>
   )
